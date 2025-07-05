0ad914368678aa43ae371fd9a900764a9554e609
jiayu chen <jiayuc267@gmail.com>
2025-07-05 16:06:13 -0700

    fixing github page

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/day-planner/', // <-- match GitHub Pages repo name
   plugins: [react()],
 });
